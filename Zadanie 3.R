99c7c35c35209d1c9e3954a1f3c9c63d76ea01f4
DominikaStododolka <d.stodolka@student.uw.edu.pl>
2025-02-23 15:45:52 +0100

    Add files via upload

---
+++ Zadanie 3.R
@@ -0,0 +1,4 @@
+#3. Stwórz funkcję o nazwie pole_kola, która oblicza pole powierzchni koła dla danego promienia.
+
+pole_kola = function(promien) {pi * promien^2}
+pole_kola(34)
---
+++ Zadanie 4.R
@@ -0,0 +1,4 @@
+# 4. Stwórz funkcję, która oblicza długość przeciwprostokątnej w trójkącie prostokątnym.
+
+przeciwprostokatna = function (bok_a, bok_b) {sqrt(bok_a^2 + bok_b^2)}
+przeciwprostokatna(4,3)
